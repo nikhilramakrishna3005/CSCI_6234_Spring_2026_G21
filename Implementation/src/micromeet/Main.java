41cfc641242d486434f54bd975175ea336d5b8b7
nikhilramakrishna3005 <nikhilrk3005@gmail.com>
2026-04-14 16:07:50 -0400

    Testing Main

--- Implementation/src/micromeet/Main.java
+++ Implementation/src/micromeet/Main.java
@@ -7,20 +7,17 @@ import micromeet.repository.UserRepository;
 
 public class Main {
     public static void main(String[] args) {
-        // Test repositories
         UserRepository userRepo = new UserRepository();
         userRepo.seedSampleUsers();
 
         MeetupRepository meetupRepo = new MeetupRepository();
         meetupRepo.seedSampleMeetups();
 
-        // Print users
         System.out.println("Users:");
         for (User u : userRepo.getAllUsers()) {
             System.out.println(u);
         }
 
-        // Print meetups
         System.out.println("\nMeetups:");
         for (Meetup m : meetupRepo.listMeetups()) {
             System.out.println(m);
