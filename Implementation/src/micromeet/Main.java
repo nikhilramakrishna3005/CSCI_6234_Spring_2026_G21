e22bf8633ab40ec8b96976843d8ac56fc5f38f06
nikhilramakrishna3005 <nikhilrk3005@gmail.com>
2026-04-14 16:12:06 -0400

    Update Main.java

--- Implementation/src/micromeet/Main.java
+++ Implementation/src/micromeet/Main.java
@@ -1,26 +1,2 @@
 package micromeet;
 
-import micromeet.entity.Meetup;
-import micromeet.entity.User;
-import micromeet.repository.MeetupRepository;
-import micromeet.repository.UserRepository;
-
-public class Main {
-    public static void main(String[] args) {
-        UserRepository userRepo = new UserRepository();
-        userRepo.seedSampleUsers();
-
-        MeetupRepository meetupRepo = new MeetupRepository();
-        meetupRepo.seedSampleMeetups();
-
-        System.out.println("Users:");
-        for (User u : userRepo.getAllUsers()) {
-            System.out.println(u);
-        }
-
-        System.out.println("\nMeetups:");
-        for (Meetup m : meetupRepo.listMeetups()) {
-            System.out.println(m);
-        }
-    }
-}
